49b0137f6c6e122f5608b5895834768007eab722
UllSif <weckering.guillaume@live.fr>
2024-07-23 15:21:38 +0200

    Fix - Corrections on Events and Form

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,6 +16,8 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        // Ajout onSuccess() si le call à fonctionné
+        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
